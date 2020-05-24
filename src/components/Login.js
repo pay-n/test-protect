9ffa91db4d8f33e5de501db9485156fb56fbcb32
Aadit Kamat <aadit.k12@gmail.com>
2020-05-24 13:32:34 +0800

    Add V2 of login page with firebase authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,32 @@
+// Import FirebaseAuth and firebase.
 import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWcY_15S_3xRn-6McM6vRADGo9V9UnGxk",
+  authDomain: "web-portal-a69d1.firebaseapp.com",
+  databaseURL: "https://web-portal-a69d1.firebaseio.com",
+  projectId: "web-portal-a69d1",
+  storageBucket: "web-portal-a69d1.appspot.com",
+  messagingSenderId: "929537871676",
+  appId: "1:929537871676:web:39b98c84f5599c004dd839"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
+  ]
+};
 
 class Login extends React.Component {
   constructor() {
@@ -10,21 +38,11 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div class="content">
-        <h1>Story</h1>
-        <p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p>
-        <p>This is a <strong>Banner</strong> element, and it's generally used as an introduction or opening statement. You can customize its <span class="demo-controls">appearance with a number of modifiers</span>, as well as assign it an optional <code>onload</code> or <code>onscroll</code> transition modifier (<a href="#reference-banner">details</a>).</p>
-        <ul class="actions stacked">
-          <li><a href="#first" class="button large wide smooth-scroll-middle">Get Started</a></li>
-        </ul>
+      <div style={{width: "100%", textAlign:"center"}}>
+        <h1> Login </h1>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
       </div>
     );
-  }
-
-  componentDidMount() {
-    this.setState({
-      someKey: 'otherValue'
-    });
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
