548fe39c3ea70c2ad0c0a579552c65e4d5cabc13
Aadit Kamat <aadit.k12@gmail.com>
2020-05-24 19:07:12 +0800

    Add basic search box

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import {
+    SearchProvider,
+    SearchBox,
+    Results
+} from '@elastic/react-search-ui';
+import { Layout } from '@elastic/react-search-ui-views';
+
+const connector = new AppSearchAPIConnector({
+    searchKey: "search-371auk61r2bwqtdzocdgutmg",
+    engineName: "search-ui-examples",
+    hostIdentifier: "host-2376rb"
+});
 
 class Search extends React.Component {
   constructor() {
@@ -9,7 +22,20 @@ class Search extends React.Component {
   }
 
   render() {
-    return <p>{this.state.someKey}</p>;
+    return (
+        <SearchProvider
+            config={{
+                apiConnector: connector
+            }}
+        >
+      <div className="App">
+        <Layout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
+    );
   }
 }
 
