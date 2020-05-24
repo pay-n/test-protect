bc84f8d9c6d27ea41f8a3698aabd4b3cf905cef6
Aadit Kamat <aadit.k12@gmail.com>
2020-05-24 18:08:54 +0800

    Add Search component

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      someKey: 'someValue'
+    };
+  }
+
+  render() {
+    return <p>{this.state.someKey}</p>;
+  }
+}
+
+export default Search;
